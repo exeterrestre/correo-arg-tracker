593c6a1f6be6a4e4f39cb327720da534e1d14ce7
Exe Terrestre <exeterrestre@gmail.com>
2025-10-28 09:52:11 -0300

    .

--- scrapper.js
+++ scrapper.js
@@ -6,7 +6,14 @@ let lastStatus = '';
 async function scrapeStatus(trackingNumber) {
     const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--single-process',
+            '--no-zygote'
+        ]
+
     });
 
     const page = await browser.newPage();
